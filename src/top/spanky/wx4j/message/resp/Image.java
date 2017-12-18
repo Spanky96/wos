d326ff2526bbacac187935c7cd3b2c1802b48abf
Spanky <spanky@SpankydeMacBook-Pro.local>
2017-12-19 00:13:38 +0800

    jj

---
+++ src/top/spanky/wx4j/message/resp/Image.java
@@ -0,0 +1,20 @@
+package top.spanky.wx4j.message.resp;
+
+/**
+ * 图片model
+ *
+ * @author liufeng
+ * @date 2013-09-11
+ */
+public class Image {
+    // 媒体文件id
+    private String MediaId;
+
+    public String getMediaId() {
+        return MediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        MediaId = mediaId;
+    }
+}
---
+++ src/top/spanky/wx4j/message/resp/VideoMessage.java
@@ -0,0 +1,20 @@
+package top.spanky.wx4j.message.resp;
+
+/**
+ * 视频消息
+ *
+ * @author liufeng
+ * @date 2013-09-11
+ */
+public class VideoMessage extends BaseMessage {
+    // 视频
+    private Video Video;
+
+    public Video getVideo() {
+        return Video;
+    }
+
+    public void setVideo(Video video) {
+        Video = video;
+    }
+}
