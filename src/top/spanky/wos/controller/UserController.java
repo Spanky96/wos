100ae00b4ed8a2704647bb384bdc07d41136f4f9
Spanky <spanky@SpankydeMacBook-Pro.local>
2017-12-19 23:42:21 +0800

    ss

--- src/top/spanky/wos/controller/UserController.java
+++ src/top/spanky/wos/controller/UserController.java
@@ -10,6 +10,10 @@ import org.springframework.web.servlet.ModelAndView;
 import top.spanky.wos.Constants;
 import top.spanky.wos.model.User;
 import top.spanky.wos.service.UserService;
+import top.spanky.wos.util.StringUtil;
+import top.spanky.wx4j.pojo.SNSUserInfo;
+import top.spanky.wx4j.pojo.WeixinOauth2Token;
+import top.spanky.wx4j.util.AdvancedUtil;
 
 @Controller
 @RequestMapping(Constants.APP_URL_PREFIX)
@@ -19,6 +23,39 @@ public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @RequestMapping(value = "/autho", method = RequestMethod.GET)
+    public ModelAndView autho(@RequestParam(value = "code", defaultValue = "") String code) {
+        ModelAndView modelAndView = new ModelAndView();
+        System.out.println(code);
+        if (StringUtil.isEmpty(code)) {
+            // 跳转到index.jsp
+            modelAndView.setViewName("index");
+            return modelAndView;
+        }
+        // 用户同意授权
+        if (!"authdeny".equals(code)) {
+            String APPID = "APPID";
+            String SECRET = "SECRET";
+            APPID = "wx107ce995902d1e0b";
+            SECRET = "efe75778beabfdd1afe1118638d22af8";
+            // 获取网页授权access_token
+            WeixinOauth2Token weixinOauth2Token = AdvancedUtil.getOauth2AccessToken(APPID, SECRET, code);
+            // 网页授权接口访问凭证
+            String accessToken = weixinOauth2Token.getAccessToken();
+            // 用户标识
+            String openId = weixinOauth2Token.getOpenId();
+            // 获取用户信息
+            SNSUserInfo snsUserInfo = AdvancedUtil.getSNSUserInfo(accessToken, openId);
+
+            // 设置要传递的参数
+            modelAndView.addObject("snsUserInfo", snsUserInfo);
+            modelAndView.setViewName("index");
+
+        }
+        // 跳转到index.jsp
+        return modelAndView;
+    }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login(@RequestParam(value = "go", defaultValue = "") String go) {
--- src/top/spanky/wx4j/util/MessageUtil.java
+++ src/top/spanky/wx4j/util/MessageUtil.java
@@ -34,44 +34,44 @@ import top.spanky.wx4j.message.resp.VoiceMessage;
  */
 public class MessageUtil {
     // 请求消息类型：文本
-    public static final String REQ_MESSAGE_TYPE_TEXT 		= "text";
+    public static final String REQ_MESSAGE_TYPE_TEXT = "text";
     // 请求消息类型：图片
-    public static final String REQ_MESSAGE_TYPE_IMAGE 		= "image";
+    public static final String REQ_MESSAGE_TYPE_IMAGE = "image";
     // 请求消息类型：语音
-    public static final String REQ_MESSAGE_TYPE_VOICE 		= "voice";
+    public static final String REQ_MESSAGE_TYPE_VOICE = "voice";
     // 请求消息类型：视频
-    public static final String REQ_MESSAGE_TYPE_VIDEO 		= "video";
+    public static final String REQ_MESSAGE_TYPE_VIDEO = "video";
     // 请求消息类型：地理位置
-    public static final String REQ_MESSAGE_TYPE_LOCATION 	= "location";
+    public static final String REQ_MESSAGE_TYPE_LOCATION = "location";
     // 请求消息类型：链接
-    public static final String REQ_MESSAGE_TYPE_LINK 		= "link";
+    public static final String REQ_MESSAGE_TYPE_LINK = "link";
 
     // 请求消息类型：事件推送
-    public static final String REQ_MESSAGE_TYPE_EVENT 		= "event";
+    public static final String REQ_MESSAGE_TYPE_EVENT = "event";
 
     // 事件类型：subscribe(订阅)
-    public static final String EVENT_TYPE_SUBSCRIBE 		= "subscribe";
+    public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
     // 事件类型：unsubscribe(取消订阅)
-    public static final String EVENT_TYPE_UNSUBSCRIBE 		= "unsubscribe";
+    public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
     // 事件类型：scan(用户已关注时的扫描带参数二维码)
-    public static final String EVENT_TYPE_SCAN 				= "scan";
+    public static final String EVENT_TYPE_SCAN = "scan";
     // 事件类型：LOCATION(上报地理位置)
-    public static final String EVENT_TYPE_LOCATION 			= "LOCATION";
+    public static final String EVENT_TYPE_LOCATION = "LOCATION";
     // 事件类型：CLICK(自定义菜单)
-    public static final String EVENT_TYPE_CLICK 			= "CLICK";
+    public static final String EVENT_TYPE_CLICK = "CLICK";
 
     // 响应消息类型：文本
-    public static final String RESP_MESSAGE_TYPE_TEXT 		= "text";
+    public static final String RESP_MESSAGE_TYPE_TEXT = "text";
     // 响应消息类型：图片
-    public static final String RESP_MESSAGE_TYPE_IMAGE 		= "image";
+    public static final String RESP_MESSAGE_TYPE_IMAGE = "image";
     // 响应消息类型：语音
-    public static final String RESP_MESSAGE_TYPE_VOICE 		= "voice";
+    public static final String RESP_MESSAGE_TYPE_VOICE = "voice";
     // 响应消息类型：视频
-    public static final String RESP_MESSAGE_TYPE_VIDEO 		= "video";
+    public static final String RESP_MESSAGE_TYPE_VIDEO = "video";
     // 响应消息类型：音乐
-    public static final String RESP_MESSAGE_TYPE_MUSIC 		= "music";
+    public static final String RESP_MESSAGE_TYPE_MUSIC = "music";
     // 响应消息类型：图文
-    public static final String RESP_MESSAGE_TYPE_NEWS 		= "news";
+    public static final String RESP_MESSAGE_TYPE_NEWS = "news";
 
     /**
      * 解析微信发来的请求（XML）
@@ -81,13 +81,13 @@ public class MessageUtil {
      * @throws Exception
      */
     @SuppressWarnings("unchecked")
-    public static Map<String, String> parseXml(HttpServletRequest request)throws Exception {
+    public static Map<String, String> parseXml(HttpServletRequest request) throws Exception {
         // 将解析结果存储在HashMap中
         Map<String, String> map = new HashMap<String, String>();
         // 从request中取得输入流
         InputStream inputStream = request.getInputStream();
         // 读取输入流
-        SAXReader reader  = new SAXReader();
+        SAXReader reader = new SAXReader();
         Document document = reader.read(inputStream);
         // 得到xml根元素
         Element root = document.getRootElement();
